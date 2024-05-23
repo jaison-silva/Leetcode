20309990e229e5392ab1f2796bda6dbec047e7dc
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2024-05-23 14:50:42 +0530

    Time: 55 ms (71.12%) | Memory: 51.1 MB (29.44%) - LeetSync

--- 202-happy-number/happy-number.js
+++ 202-happy-number/happy-number.js
@@ -3,26 +3,25 @@
  * @return {boolean}
  */
 var isHappy = function(n) {
-        function leetcode(n,seen= new Set()){
 
-        if(n==1) {
-            return true
-        }else if(seen.has(n)){
-                return false
-        }
-        seen.add(n)
+    let cycle = new Set()
+    let arr = n.toString().split('')
 
-        let arr = n.toString().split("")
-        console.log(arr)
-
+    while(true){
+        
         let output = arr.reduce((acc,val)=>{
-            return acc + Math.pow(parseInt(val),2)
+            return acc += val*val
         },0)
 
-        console.log("this is the ouptyu",output)
+        if(output == 1 ){
+            return true
+        }
 
-        return leetcode(output,seen)
+        if(cycle.has(output)){
+            return false
+        }
+        cycle.add(output)
+        arr = output.toString().split('')
     }
 
-    return leetcode(n)
 };
