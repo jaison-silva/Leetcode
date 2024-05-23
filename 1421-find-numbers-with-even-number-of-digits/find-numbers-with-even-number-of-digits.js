b57d247fe8e10df7a7fddc5e2c59bd37df868e96
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2024-05-23 17:06:59 +0530

    Time: 52 ms (72.64%) | Memory: 50.6 MB (33.31%) - LeetSync

--- 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
+++ 1421-find-numbers-with-even-number-of-digits/find-numbers-with-even-number-of-digits.js
@@ -3,11 +3,11 @@
  * @return {number}
  */
 var findNumbers = function(nums) {
-    let count = 0;
-    for(let i = 0; i < nums.length; i++) {
-        if((nums[i].toString().length) % 2 === 0) {
-            count++;
+    let count = 0
+    for(let i = 0; i<nums.length;i++){
+        if((nums[i].toString().length)%2==0){
+            count ++
         }
     }
-    return count;
+    return count
 };
