85b91cc670bd174d83c8f84ec3b1c7e0cf465ee0
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2024-08-01 11:28:01 +0530

    Time: 61 ms (50.28%) | Memory: 50.7 MB (54.24%) - LeetSync

--- 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
+++ 1904-second-largest-digit-in-a-string/second-largest-digit-in-a-string.js
@@ -3,43 +3,25 @@
  * @return {number}
  */
 var secondHighest = function(s) {
-    let temp = new Set()
-    for (let i = 0; i < s.length; i++) {
-        // console.log("blah 1")
-        if (!(((s[i].charCodeAt(0)) >= 65 && (s[i].charCodeAt(0)) <= 90) || ((s[i].charCodeAt(0)) >= 97 && (s[i].charCodeAt(0)) <= 122))) {
-            temp.add(s[i])
+    let uniqueNumbers = new Set()
+
+    for(let n of s){
+        if(!isNaN(n)){
+            uniqueNumbers.add(n)
         }
     }
-
-    // console.log(temp)
-    let size = temp.size
-    let arr = [...temp]
-
-    let large = arr[0]
-    let s_lar = 0
-
-    for (let i = 0; i < size; i++) {
-        // console.log(s[i])
-        if (arr[i] > large) {
-            // console.log("blah 3")
-            large = arr[i]
-        }
-    }
-
-    for (let i = 0; i < size; i++) {
-        // console.log(s[i])
-        if (arr[i] > s_lar && arr[i]<large) {
-            // console.log("blah 3")
-            s_lar = arr[i]
-        }
-    }
-
-    // console.log("arr size"+size)
-    console.log(arr,large,s_lar)
-
-    if (arr.length <= 1) {
+    
+    if(uniqueNumbers.length == 1){
         return -1
-    }else{
-        return s_lar
     }
+
+    let arr = Array.from(uniqueNumbers)
+    arr.sort((a,b)=>a-b)
+
+    const size = arr.length
+
+    if(size<2){
+        return -1
+    }
+    return arr[size-2]
 };
