5fdc2d351526c8be340ed5344e9fed2ba1e3a7eb
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2024-09-28 20:36:36 +0530

    Time: 63 ms (20.77%) | Memory: 51.7 MB (15.46%) - LeetSync

--- 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
+++ 1476-count-negative-numbers-in-a-sorted-matrix/count-negative-numbers-in-a-sorted-matrix.js
@@ -2,15 +2,25 @@
  * @param {number[][]} grid
  * @return {number}
  */
-var countNegatives = function(grid) {
-    let count = 0
+var countNegatives = function (grid) {
+    // let count = 0
 
-    for(let i of grid){
-        for(let j of i){
-            if(j<0){
-                count ++
-            }
-        }
-    }
-    return count
+    // for(let i of grid){
+    //     for(let j of i){
+    //         if(j<0){
+    //             count ++
+    //         }
+    //     }
+    // }
+    // return count
+
+    // let count = 0
+    // return grid.flat().filter(val => val < 0   ).length
+    // return count
+
+    return grid.flat().reduce((acc,val)=>{
+        return val<0 ? ++acc : acc
+    },0)
 };
+
+
