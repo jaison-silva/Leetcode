07387f07af2b316c7fc0ddaed4c2a15da3b25897
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2024-10-24 17:25:21 +0530

    Time: 0 ms (100.00%) | Memory: 48.8 MB (79.36%) - LeetSync

--- 1646-kth-missing-positive-number/kth-missing-positive-number.js
+++ 1646-kth-missing-positive-number/kth-missing-positive-number.js
@@ -3,23 +3,40 @@
  * @param {number} k
  * @return {number}
  */
-var findKthPositive = function(arr, k) {
+var findKthPositive = function (arr, k) {
     // const limit = Math.max(...arr)
 
-    let missing = new Set()
+    // let missing = new Set()
 
-    let i = 0
+    // let i = 0
 
-    while(i<=10000){
-        missing.add(i)
+    // while(i<=10000){
+    //     missing.add(i)
+    //     i++
+    // }
+
+    // for(let i = 0; i<arr.length;i++){
+    //     missing.delete(arr[i])
+    // }
+
+    // // return missing[k]
+    // let result = [...missing]
+    // return result[k]
+
+  let missingNum = 0
+  let currNum = 1
+  let i = 0
+
+  while(missingNum<k){
+    if(i>=arr.length || arr[i]!==currNum){
+        missingNum++
+        if(missingNum === k){
+            return currNum
+        }
+    }else{
         i++
     }
+    currNum++
+  }
 
-    for(let i = 0; i<arr.length;i++){
-        missing.delete(arr[i])
-    }
-
-    // return missing[k]
-    let result = [...missing]
-    return result[k]
 };
