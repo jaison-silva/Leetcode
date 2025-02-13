30efc452eba963c039b6dbfb8ecc9d31a4578001
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-02-13 13:35:35 +0530

    Time: 13 ms (24.11%) | Memory: 58.9 MB (46.59%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -2,20 +2,16 @@
  * @param {number} x
  * @return {boolean}
  */
-var isPalindrome = function(x) {
-    let nums = ''
-    let check = x.toString()
-    if(check.length<=1){
-        return true
-    }else{
-        for(let i = check.length-1;i>=0;i--){
-        nums += check[i]
-    }
-    }
+var isPalindrome = function (x) {
+    // console.log(x.toString().split().reverse().join())
+    const val = x.toString()
 
-    if(nums==check){
+    if(val == val.split('').reverse().join('')) {
         return true
-    }else{
+    } else {
         return false
     }
+
+    let string = x.toString()
+    console.log(string)
 };
