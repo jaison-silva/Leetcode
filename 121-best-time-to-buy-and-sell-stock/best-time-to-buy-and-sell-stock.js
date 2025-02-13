61586fcac45063c542922553e56693a71866509a
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-02-13 12:18:40 +0530

    Time: 6 ms (17.51%) | Memory: 59.6 MB (26.83%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -3,20 +3,13 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-  let min_price = prices[0]
-  let max_price = 0
-  let max_profit = 0
+    let minPrice = Infinity
+    let maxProfit = 0
 
-  let n = prices.length
+    for(const val of prices){
+        minPrice = Math.min(minPrice,val)
+        maxProfit = Math.max(maxProfit,val-minPrice)
+    }
 
-  if(n==0){
-      return 0
-  }
-
-  for(let i = 0 ; i<n ; i++){
-      min_price = Math.min(prices[i],min_price)
-      max_profit = Math.max(max_profit, prices[i]-min_price )
-  }
-
-  return max_profit
+    return maxProfit
 };
