64e09d81b58f77665305eb575ccfd1ff406611bb
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-02-22 11:48:45 +0530

    Time: 215 ms (8.91%) | Memory: 54.7 MB (12.51%) - LeetSync

--- 268-missing-number/missing-number.js
+++ 268-missing-number/missing-number.js
@@ -5,14 +5,10 @@
 var missingNumber = function(nums) {
     let n = nums.length
 
-    // if(n==1){
-    //     if(n==0)
-    // }
-
-    let unique = new Set(nums)
+    // let unique = new Set(nums)
 
     for(let i = 1; i<=n; i++){
-        if(!(unique.has(i))){
+        if(!(nums.includes(i))){
             return i
         }
     }
