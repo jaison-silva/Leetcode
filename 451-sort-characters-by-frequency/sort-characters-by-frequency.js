ad2fcf96b84f8b35784884960b4c7e9bfaedfd3f
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-03-10 10:47:59 +0530

    Time: 15 ms (66.70%) | Memory: 58 MB (51.51%) - LeetSync

--- 451-sort-characters-by-frequency/sort-characters-by-frequency.js
+++ 451-sort-characters-by-frequency/sort-characters-by-frequency.js
@@ -6,17 +6,19 @@ var frequencySort = function (s) {
 
     let freqMap = {}
 
-    for (let val of s) {
-        if (freqMap[val]) {
-            freqMap[val]++
-        } else {
-            freqMap[val] = 1
-        }
+    // for (let val of s) {
+    //     if (freqMap[val]) {
+    //         freqMap[val]++
+    //     } else {
+    //         freqMap[val] = 1
+    //     }
+    // }
+
+    for(let val of s){
+        freqMap[val] = (freqMap[val] || 0) + 1
     }
 
-    console.log(freqMap)
-
-    let result = Object.entries(freqMap).sort((a, b) => b[1] - a[1]).map(([val,freq])=>val.repeat(freq)).join('')
+    let result = Object.entries(freqMap).sort((a, b) => b[1] - a[1]).map(([val, freq]) => val.repeat(freq)).join('')
 
     return result
 };
