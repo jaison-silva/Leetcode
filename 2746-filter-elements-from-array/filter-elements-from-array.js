825204d341194af8a519ac1803f8fc96394cdf42
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-03-24 20:34:34 +0530

    Time: 45 ms (61.89%) | Memory: 53.6 MB (33.51%) - LeetSync

--- 2746-filter-elements-from-array/filter-elements-from-array.js
+++ 2746-filter-elements-from-array/filter-elements-from-array.js
@@ -4,11 +4,9 @@
  * @return {number[]}
  */
 var filter = function(arr, fn) {
-    let filteredArr=[]
-    for(let i=0;i<arr.length;i++){
-        if(fn(arr[i],i)){
-            filteredArr.push(arr[i]);
-        }
-    }
-    return filteredArr
+    const result = arr.filter((val,index)=>{
+       return fn(val,index)
+    })
+
+    return result
 };
