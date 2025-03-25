cb6c41cb51c091530081d0c31e6103748cece5a1
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-03-25 10:45:01 +0530

    Time: 4 ms (90.28%) | Memory: 64.7 MB (66.86%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.js
@@ -5,7 +5,8 @@
 var findDuplicates = function (nums) {
 
     let duplicates = []
-    const seen = new Set()
+
+    // const seen = new Set()
 
     // for (let i = 0; i < nums.length; i++) {
     //     for (let j = i + 1; j < nums.length; j++) {
@@ -15,12 +16,22 @@ var findDuplicates = function (nums) {
     //     }
     // }
 
-    for (let val of nums) {
-        if (seen.has(val)){
-            duplicates.push(val)
-        } else {
-            seen.add(val)
+    // for (let val of nums) {
+    //     if (seen.has(val)){
+    //         duplicates.push(val)
+    //     } else {
+    //         seen.add(val)
+    //     }
+    // }
+
+    for(let i = 0; i<nums.length; i++){
+        let index = Math.abs(nums[i]) - 1
+
+        if(nums[index]<0){
+            duplicates.push(index+1)
         }
+
+        nums[index] = -nums[index]
     }
 
     return duplicates
