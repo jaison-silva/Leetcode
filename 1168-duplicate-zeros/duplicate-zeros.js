af6ca5279e208d61ee24b6e5d63fb8fbec884903
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-05-05 17:48:28 +0530

    Time: 10 ms (22.07%) | Memory: 55.7 MB (88.60%) - LeetSync

--- 1168-duplicate-zeros/duplicate-zeros.js
+++ 1168-duplicate-zeros/duplicate-zeros.js
@@ -3,16 +3,26 @@
  * @return {void} Do not return anything, modify arr in-place instead.
  */
 var duplicateZeros = function(arr) {
-    const length = arr.length
+    // const length = arr.length
 
-    for(let i = 0 ; i<length; i++){
-        if(arr[i]==0){
-            arr.splice(i,0,0)
-            i++
-        }
-    }
+    // for(let i = 0 ; i<length; i++){
+    //     if(arr[i]==0){
+    //         arr.splice(i,0,0)
+    //         i++
+    //     }
+    // }
 
-    console.log("arr  "+arr)
-    // console.log("index  "+index)
-    arr.splice(length) 
+    // console.log("arr  "+arr)
+    // arr.splice(length) 
+
+    for(let i=0;i<arr.length-1;i++){
+        if(arr[i]==0) {
+            for(let j=arr.length-1;j>=i+1;j--){
+                arr[j] = arr[j-1];
+            }
+            arr[i+1] = 0;
+            i++;
+        }
+    }
 };
+
