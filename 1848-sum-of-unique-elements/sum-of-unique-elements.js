50099753229e8f8b6e2e36ff4632ca03447629a2
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-05-08 09:20:18 +0530

    Time: 7 ms (5.08%) | Memory: 54.4 MB (25.21%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.js
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.js
@@ -5,30 +5,27 @@
 var sumOfUnique = function (nums) {
     let unique = []
 
-    for (let i = 0; i < nums.length; i++) {
-        let dup = false
-        for (let j = 0; j < nums.length; j++) {
-            if (nums[i] == nums[j] && i !== j) {
-                dup = true
-                break
-            }
-        }
-        if (!dup) {
-            unique.push(nums[i])
-        }
-    }
-
-    return unique.reduce((acc,val)=>acc+val,0)
-
     // for (let i = 0; i < nums.length; i++) {
-    //     let val = nums.pop()
-    //     if (!nums.includes(val)) {
-    //         unique.push(val)
+    //     let dup = false
+    //     for (let j = 0; j < nums.length; j++) {
+    //         if (nums[i] == nums[j] && i !== j) {
+    //             dup = true
+    //             break
+    //         }
+    //     }
+    //     if (!dup) {
+    //         unique.push(nums[i])
     //     }
     // }
 
 
+    for (let i = 0; i < nums.length; i++) {
+        if (nums.indexOf(nums[i]) == nums.lastIndexOf(nums[i])) { 
+            unique.push(nums[i])
+        }
+    }
 
     console.log(unique)
+    return unique.reduce((acc,val)=>acc+val,0)
 
 };
