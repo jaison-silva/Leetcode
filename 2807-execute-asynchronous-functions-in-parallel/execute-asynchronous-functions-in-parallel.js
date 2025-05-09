3b0a7d971b940ba73edca2db1358496b1b429126
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-05-09 10:58:52 +0530

    Time: 56 ms (78.36%) | Memory: 54.9 MB (16.29%) - LeetSync

---
+++ 2807-execute-asynchronous-functions-in-parallel/execute-asynchronous-functions-in-parallel.js
@@ -0,0 +1,33 @@
+/**
+ * @param {Array<Function>} functions
+ * @return {Promise<any>}
+ */
+var promiseAll = function(functions) {
+    return new Promise((resolve,reject)=>{
+        const results = []
+        let completed = 0
+
+        functions.forEach((fn,index)=>{
+
+            fn()
+            .then((result)=>{
+                completed += 1
+                results[index] = result
+
+                if(completed == functions.length){
+                    resolve(results)
+                }
+            })
+            .catch((reason)=>{
+                completed += 1
+                reject(reason)
+            })
+
+        })
+    })
+};
+
+/**
+ * const promise = promiseAll([() => new Promise(res => res(42))])
+ * promise.then(console.log); // [42]
+ */
