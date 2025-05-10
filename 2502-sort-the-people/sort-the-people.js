ae0608f7c454d1b36a0842be786e1b1db06adf38
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-05-10 17:01:14 +0530

    Time: 4 ms (97.31%) | Memory: 61.1 MB (15.04%) - LeetSync

--- 2502-sort-the-people/sort-the-people.js
+++ 2502-sort-the-people/sort-the-people.js
@@ -4,5 +4,5 @@
  * @return {string[]}
  */
 var sortPeople = function(names, heights) {
-    return names.map((val,index)=>[val,heights[index]]).sort((a,b)=>b[1]-a[1]).map((val)=>val[0])
+    return names.map((val,index)=>[val,heights[index]]).sort((a,b)=>b[1]-a[1]).map(([name])=>name)
 };
