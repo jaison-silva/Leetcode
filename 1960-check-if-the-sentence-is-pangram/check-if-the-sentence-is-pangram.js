04d70392dad6dba1285b7574ff03e64639b6fd64
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-05-27 16:21:56 +0530

    Time: 2 ms (62.54%) | Memory: 55.4 MB (32.09%) - LeetSync

--- 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
+++ 1960-check-if-the-sentence-is-pangram/check-if-the-sentence-is-pangram.js
@@ -3,22 +3,5 @@
  * @return {boolean}
  */
 var checkIfPangram = function(sentence) {
-    const given = sentence.split('')
-
-    let all = 'abcdefghijklmnopqrstuvwxyz'.split('')
-    // console.log(given)
-
-    // all.map((val)=>{
-    //     if(!(given.includes(val))){
-    //         return false
-    //     }
-    // })
-
-    for(let i = 0; i<all.length; i++){
-        if(!given.includes(all[i])){
-            return false
-        }
-    }
-
-    return true
+    return new Set(sentence).size === 26 ? true : false
 };
