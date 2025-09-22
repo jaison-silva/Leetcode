420ef59b366f82c200eca9404626ebcb2c0bdcf6
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-09-22 16:37:22 +0530

    Time: 57 ms (14.29%) | Memory: 55.4 MB (34.03%) - LeetSync

--- 3311-ant-on-the-boundary/ant-on-the-boundary.js
+++ 3311-ant-on-the-boundary/ant-on-the-boundary.js
@@ -4,35 +4,13 @@
  */
 var returnToBoundaryCount = function (nums) {
 
-    // nums.map((val)=>{
-    //     if(val<0){
-    //         curr = curr-val
-    //     }else{
-    //         curr = curr+val
-    //     }
+    let curr = 0
 
-    //     if(val == 0){
-    //         returnVal++
-    //     }
-    // })
+    return nums.reduce((acc,val)=>{
+        curr += val
 
-    let curr = nums[0]
-    let ant = 0
-
-    for (let i = 1; i < nums.length; i++) {
-
-        if (nums[i] < 0) {
-            curr = curr - Math.abs(nums[i])
-        } else {
-            curr = curr + nums[i]
-        }
-
-        console.log(curr)
-        if (curr == 0) {
-            ant++
-        }
-
-    }
-
-    return ant
+        if(curr == 0){
+            return ++acc
+        }else return acc
+    },0)
 };
