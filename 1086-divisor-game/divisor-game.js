3e070ac6b98345c88c93547bd63f01fdd3a578cb
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-10-28 16:25:30 +0530

    Time: 0 ms (100.00%) | Memory: 52.6 MB (97.00%) - LeetSync

--- 1086-divisor-game/divisor-game.js
+++ 1086-divisor-game/divisor-game.js
@@ -2,12 +2,6 @@
  * @param {number} n
  * @return {boolean}
  */
-var divisorGame = function (n) {
-    let turn = "Alice"
-
-    if (n % 2 == 0) {
-        return true
-    } else {
-        return false
-    }
+var divisorGame = function(n) {
+    return n%2==0
 };
