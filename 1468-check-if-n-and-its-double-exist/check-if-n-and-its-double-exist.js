a875d548b37f81b99a767cfc34cf8e25fb3ebcb7
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-11-07 09:57:21 +0530

    Time: 1 ms (74.05%) | Memory: 55.3 MB (58.17%) - LeetSync

--- 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -3,15 +3,12 @@
  * @return {boolean}
  */
 var checkIfExist = function(arr) {
-    let i,j = 0
-
-    for(let i=0; i<arr.length; i++){
-        for(let j=0; j<arr.length; j++){
-            if(i!=j && arr[i] == 2* arr[j]){
+    for(let i = 0; i<arr.length; i++){
+        for(let j = 0; j<arr.length; j++){
+            if(i!=j && arr[i]==2*arr[j]){
                 return true
             }
         }
     }
-
     return false
 };
