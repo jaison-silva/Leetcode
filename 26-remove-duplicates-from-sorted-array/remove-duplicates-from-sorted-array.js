5434c10a11c7d183f49e1aece86567fa35b0d709
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-11-10 16:52:25 +0530

    Time: 0 ms (100.00%) | Memory: 56.3 MB (95.69%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.js
@@ -2,15 +2,15 @@
  * @param {number[]} nums
  * @return {number}
  */
-var removeDuplicates = function(nums) {
+var removeDuplicates = function (nums) {
     let i = 0
 
-    for(let j=i+1; j<nums.length; j++){
-        if(nums[i] !== nums[j]){
+    for (let j = 1; j < nums.length; j++) {
+        if (nums[i] != nums[j]) {
             i++
-            nums[i]=nums[j]
+            nums[i] = nums[j]
         }
     }
 
     return i+1
-};
+};  
