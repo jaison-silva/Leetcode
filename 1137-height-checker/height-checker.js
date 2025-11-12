de260430365da2b858594c7d50bb11c6bc13d555
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-11-12 18:36:09 +0530

    Time: 1 ms (55.34%) | Memory: 54.4 MB (62.71%) - LeetSync

---
+++ 1137-height-checker/height-checker.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} heights
+ * @return {number}
+ */
+var heightChecker = function(heights) {
+    const expected = [...heights]
+    expected.sort((a,b)=>a-b)
+    let result = 0
+    for(let i = 0; i<heights.length;i++){
+        if(heights[i]!=expected[i]) result++
+    }
+    return result
+};
