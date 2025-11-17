3a9595ca01c1e1d8fd7c974e84d2359e1eb38165
Jaison joy silva <85454770+jaison-silva@users.noreply.github.com>
2025-11-17 14:47:28 +0530

    Time: 107 ms (51.79%) | Memory: 77.3 MB (69.78%) - LeetSync

--- 948-sort-an-array/sort-an-array.js
+++ 948-sort-an-array/sort-an-array.js
@@ -2,33 +2,40 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
+var sortArray = function (nums) {
 
-    return (function mergeSort(nums){
-    if(nums.length <=1) return nums
+    function bubbleDown(i,length = nums.length) {
+        let index = i
+        // let length = nums.length
 
-        let mid = Math.floor(nums.length/2)
-        let left = mergeSort(nums.slice(0,mid))  
-        let right = mergeSort(nums.slice(mid))
+        while (true) {
+            let largest = index
+            let left = index * 2 + 1
+            let right = index * 2 + 2
 
-        return merge(left,right)
-    })(nums)
-
-    function merge(left,right){
-        let result = []
-        let i = 0
-        let j = 0
-
-        while(i<left.length && j<right.length){ 
-            if(left[i]<right[j]){
-                result.push(left[i++])
-            }else{
-                result.push(right[j++])
+            if (left < length && nums[left] > nums[largest]) {
+                largest = left
             }
-        }
+            if (right < length && nums[right] > nums[largest]) {
+                largest = right
+            }
 
-        return [...result,...left.slice(i),...right.slice(j)]
+            if (largest == index) break; 
+
+            [nums[index], nums[largest]] = [nums[largest], nums[index]]
+
+            index = largest
+        }
     }
 
-    // return mergeSort(nums)
+    for (let i = Math.floor(((nums.length) / 2) - 1); i >= 0; i--) { // build heap
+        bubbleDown(i)
+    }
+
+    for(let i = nums.length-1; i>0; i--){
+        [nums[0], nums[i]] = [nums[i], nums[0]]
+        bubbleDown(0,i)
+    }
+
+    return nums
 };
